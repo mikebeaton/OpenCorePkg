f399edbedb298c1fd9febe4e3760513216c79c7d
MikeBeaton <mjsbeaton@gmail.com>
2020-12-08 21:09:03 +0000

    CheckSchema_h.py: allow ignore pairs

--- Library/OcConfigurationLib/CheckSchema_h.py
+++ Library/OcConfigurationLib/CheckSchema_h.py
@@ -8,7 +8,7 @@ import re
 import sys
 
 if len(sys.argv) < 2:
-  print('Pass file to check')
+  print('Pass file to check, followed by optional pairs to ignore as "FirstTag;SecondTag"')
   sys.exit(-1)
 
 with open(sys.argv[1], 'r') as f:
@@ -23,6 +23,12 @@ with open(sys.argv[1], 'r') as f:
     x = re.search(r'_\(\w+\s*,\s*(\w+)', l)
     if x:
       if x.group(1) < prev:
-        print('ERROR: {} precedes {}'.format(prev, x.group(1)))
-        sys.exit(1)
+        ignore = False
+        for i in range(2, len(sys.argv)):
+          if '%s;%s' % (prev, x.group(1)) == sys.argv[i]:
+            ignore = True
+            break
+        if not ignore:
+          print('ERROR: {} precedes {}'.format(prev, x.group(1)))
+          sys.exit(1)
       prev = x.group(1)
