3a6fc7b7aca0d354a9d2de0e9c12310ff6c0d788
MikeBeaton <mjsbeaton@gmail.com>
2020-12-27 10:22:38 +0000

    asdsa

--- Include/Acidanthera/Library/OcConfigurationLib.h
+++ Include/Acidanthera/Library/OcConfigurationLib.h
@@ -563,14 +563,14 @@
 /// Audio is a set of options for sound configuration.
 ///
 #define OC_UEFI_AUDIO_FIELDS(_, __) \
-  _(UINT8                       , AudioCodec         ,     , 0                             , ()) \
-  _(OC_STRING                   , AudioDevice        ,     , OC_STRING_CONSTR ("", _, __)  , OC_DESTR (OC_STRING)) \
-  _(UINT8                       , AudioOut           ,     , 0                             , ()) \
-  _(BOOLEAN                     , AudioSupport       ,     , FALSE                         , ()) \
-  _(UINT8                       , MinimumVolume      ,     , 0                             , ()) \
-  _(OC_STRING                   , PlayChime          ,     , OC_STRING_CONSTR ("", _, __)  , OC_DESTR (OC_STRING)) \
-  _(UINTN                       , SetupDelay         ,     , 0                             , ()) \
-  _(UINT16                      , VolumeAmplifier    ,     , 0                             , ())
+  _(UINT8                       , AudioCodec         ,     , 0                             , () ) \
+  _(OC_STRING                   , AudioDevice        ,     , OC_STRING_CONSTR ("", _, __)  , OC_DESTR (OC_STRING) ) \
+  _(UINT8                       , AudioOut           ,     , 0                             , () ) \
+  _(BOOLEAN                     , AudioSupport       ,     , FALSE                         , () ) \
+  _(UINT8                       , MinimumVolume      ,     , 0                             , () ) \
+  _(OC_STRING                   , PlayChime          ,     , OC_STRING_CONSTR ("", _, __)  , OC_DESTR (OC_STRING) ) \
+  _(UINTN                       , SetupDelay         ,     , 0                             , () ) \
+  _(UINT16                      , VolumeAmplifier    ,     , 0                             , () )
   OC_DECLARE (OC_UEFI_AUDIO)
 
 ///
