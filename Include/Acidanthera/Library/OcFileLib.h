0a93d0897801921a07fa07a6a50ae7462f00a4c9
MikeBeaton <mjsbeaton@gmail.com>
2022-06-14 11:42:45 +0100

    more

--- Include/Acidanthera/Library/OcFileLib.h
+++ Include/Acidanthera/Library/OcFileLib.h
@@ -108,7 +108,7 @@ OcGetVolumeLabel (
 
   @param  Directory  File protocol instance of parent directory.
   @param  NewHandle  Pointer for returned handle.
-  @param  FileName   Null-terminated file name.
+  @param  FileName   Null-terminated file name or relative path.
   @param  OpenMode   File open mode.
   @param  Attributes Attributes for the newly created file.
 
@@ -127,7 +127,7 @@ OcSafeFileOpen (
   Report existence of file relative to source file's location.
 
   @param  Directory  File protocol instance of parent directory.
-  @param  FileName   Null-terminated file name.
+  @param  FileName   Null-terminated file name or relative path.
 
   @retval TRUE when file exists.
 **/
@@ -141,7 +141,7 @@ OcFileExists (
   Delete child file relative to source file's location.
 
   @param  Directory  File protocol instance of parent directory.
-  @param  FileName   Null-terminated file name.
+  @param  FileName   Null-terminated file name or relative path.
 
   @retval EFI_SUCCESS     File successfully deleted.
   @retval EFI_NOT_FOUND   File was not present.
